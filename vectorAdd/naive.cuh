32e263d9313c0d22ae9a00babcf0555041d29a27
Jackiempty <candypaper0123@gmail.com>
2025-09-19 02:04:18 +0800

    feat: Refactor vectorAdd to a independent program that calls GPU APIs in its own main()

--- vectorAdd/naive.cuh
+++ vectorAdd/naive.cuh
@@ -1,16 +1,18 @@
 #include <cuda_runtime.h>
 
-__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+__global__ void vector_add_naive(const float* A, const float* B, float* C, int N) {
     for (int i = 0; i < N; i++) {
         C[i] = A[i] + B[i];
     }
 }
 
+
+
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
-extern "C" void solve(const float* A, const float* B, float* C, int N) {
+void naive(const float* A, const float* B, float* C, int N) {
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    vector_add_naive<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
     cudaDeviceSynchronize();
 }
--- vectorAdd/optimized.cuh
+++ vectorAdd/optimized.cuh
@@ -1,6 +1,6 @@
 #include <cuda_runtime.h>
 
-__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+__global__ void vector_add_optimized(const float* A, const float* B, float* C, int N) {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (i < N) {
@@ -9,10 +9,10 @@ __global__ void vector_add(const float* A, const float* B, float* C, int N) {
 }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
-extern "C" void solve(const float* A, const float* B, float* C, int N) {
+void optimized(const float* A, const float* B, float* C, int N) {
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    vector_add_optimized<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
     cudaDeviceSynchronize();
 }
